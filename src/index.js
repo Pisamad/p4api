cafce235aadc02c047f517f2ac07f7f7be01f3b4
Pisamad <pierre.duot@gmail.com>
2018-09-20 09:46:21 +0200

    Create LICENSE

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2018 Pisamad. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
 import Q from 'bluebird';
 import _ from 'lodash';
 import {spawn, spawnSync} from 'child_process';
