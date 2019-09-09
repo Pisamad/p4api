47a579c9d575636f186c8b82e2c8f4da09912945
Pisamad <848940+Pisamad@users.noreply.github.com>
2019-09-09 11:51:02 +0200

    Raw (#9)
    * Add raw methods for V3.3.0
    
    * Change result field for raw commands

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    node: true
+  },
+  extends: 'standard',
+  globals: {
+    __static: true,
+    store: true
+  },
+  plugins: [],
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
--- RELEASE.md
+++ RELEASE.md
@@ -1,3 +1,7 @@
+#V3.3.0
+## Major Features and Improvements
+* Add rawCmd & rawCmdSync command
+
 #V3.2.0
 ## Major Features and Improvements
 * Add p4.cmd cancellation feature
